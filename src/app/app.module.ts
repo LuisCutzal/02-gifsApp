c601d835b4e229a33e1b3a0a56385ab3632db661
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-06 18:45:29 -0600

    Realizar peticiones HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -11,9 +11,10 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
-    GifsModule
+    HttpClientModule,
     
+    SharedModule,
+    GifsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,20 @@
+import { query } from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  private apiKey:string="QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs";
   private _historial:string[]=[];
+  public resultados:any[]=[];
   get historial(){
     
     return [...this._historial];//llaves indicando q es un arreglo, los 3 puntos significa que el operador es spread, separa cada uno de los elementos y crea uno nuevo
   }
+
+  constructor(private http: HttpClient){ }
 
   buscarGifs( terminoBusqueda:string){
     terminoBusqueda=terminoBusqueda.trim().toLowerCase();
@@ -18,9 +24,11 @@ export class GifsService {
     //this._historial.push(terminoBusqueda);//para incertarlo al final del arreglo
     this._historial=this._historial.splice(0,10); //para cortar el historial y solo sean 10 elementos que se ven
     }
-    
-    
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs&q=${terminoBusqueda}&limit=10`)
+          .subscribe( (resp:any)=>{
+            console.log(resp.data);
+            this.resultados=resp.data;
+          });
   }
 
 }
