89f97fe53379da71fcc4d5c91f47300eafaef944
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-06 20:13:53 -0600

    de nuevo el httpParams ya que la "k" de api_key estaba en mayuscula y eso da error, y se coloco un estilo de animacion para cuando cargue una busqueda se vea boniot

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,13 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  private apiKey:string="QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs";
-  private serviciosUrl:string="https://api.giphy.com/v1/gifs";
+  private apiKey:string='QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs';
+  private servicioUrl:string= 'https://api.giphy.com/v1/gifs';
   private _historial:string[]=[];
   public resultados:Gif[]=[];
   get historial(){
@@ -37,14 +37,16 @@ export class GifsService {
     this._historial=this._historial.splice(0,10); //para cortar el historial y solo sean 10 elementos que se ven
 
     localStorage.setItem("historial",JSON.stringify(this._historial));
-    
     }
-    const params=new HttpParams()
-    .set("apiKey",this.apiKey)
-    .set("limit","10")
-    .set("terminoBusqueda",terminoBusqueda);
-    console.log(params.toString());
-    this.http.get<SearchGifsResponse>(`${this.serviciosUrl}/serch`,{params})
+
+const params = new HttpParams()
+          .set('api_key', this.apiKey) //tener en cuenta las mayusculas y minusculas
+          .set('limit', '10')
+          .set('q',terminoBusqueda );
+
+
+  
+   this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
           .subscribe( (resp)=>{
             console.log(resp.data);
             this.resultados=resp.data;
