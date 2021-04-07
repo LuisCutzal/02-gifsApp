f5fe03eb7e1ad09552aa727de10e138b58191ff2
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-06 18:55:28 -0600

    tipados para la peticion HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,6 @@
-import { query } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private apiKey:string="QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs";
   private _historial:string[]=[];
-  public resultados:any[]=[];
+  public resultados:Gif[]=[];
   get historial(){
     
     return [...this._historial];//llaves indicando q es un arreglo, los 3 puntos significa que el operador es spread, separa cada uno de los elementos y crea uno nuevo
@@ -24,8 +24,8 @@ export class GifsService {
     //this._historial.push(terminoBusqueda);//para incertarlo al final del arreglo
     this._historial=this._historial.splice(0,10); //para cortar el historial y solo sean 10 elementos que se ven
     }
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs&q=${terminoBusqueda}&limit=10`)
-          .subscribe( (resp:any)=>{
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs&q=${terminoBusqueda}&limit=10`)
+          .subscribe( (resp)=>{
             console.log(resp.data);
             this.resultados=resp.data;
           });
