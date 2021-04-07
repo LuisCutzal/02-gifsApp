a22a43fb87712c8d13a49dc9c3df4884aa2f4b34
LuisCutzal <70243302+LuisCutzal@users.noreply.github.com>
2021-04-06 19:12:34 -0600

    localstorage= hacer percistente la informacion de la busqueda de las imagenes en el localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,17 @@ export class GifsService {
     return [...this._historial];//llaves indicando q es un arreglo, los 3 puntos significa que el operador es spread, separa cada uno de los elementos y crea uno nuevo
   }
 
-  constructor(private http: HttpClient){ }
+  constructor(private http: HttpClient){ 
+
+    //forma 1
+    /*if (localStorage.getItem("historial")) {
+      this._historial=JSON.parse(localStorage.getItem("historial")!);      
+    }*/
+
+    //forma 2
+    this._historial=JSON.parse(localStorage.getItem("historial")!) || [];
+
+  }
 
   buscarGifs( terminoBusqueda:string){
     terminoBusqueda=terminoBusqueda.trim().toLowerCase();
@@ -23,6 +33,8 @@ export class GifsService {
       this._historial.unshift(terminoBusqueda);//para incertarlo al inicio del arreglo
     //this._historial.push(terminoBusqueda);//para incertarlo al final del arreglo
     this._historial=this._historial.splice(0,10); //para cortar el historial y solo sean 10 elementos que se ven
+
+    localStorage.setItem("historial",JSON.stringify(this._historial));
     }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=QaiPXKFFAy1DF7hBiYoEuubB9ZjdsdQs&q=${terminoBusqueda}&limit=10`)
           .subscribe( (resp)=>{
